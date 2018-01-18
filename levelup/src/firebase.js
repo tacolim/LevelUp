771a81b84eedc9476c6a03dab219280ee5e29e27
Emily Sperry <sperrye@gmail.com>
2018-01-18 14:42:27 -0800

    Added config template

--- levelup/src/firebase.js
+++ levelup/src/firebase.js
@@ -1,7 +1,13 @@
 import firebase from 'firebase';
     // Initialize Firebase
     const config = {
-
+        apiKey: "Your-API-Key",
+           authDomain: "your-project-id.firebaseapp.com",
+           databaseURL: "https://your-project-id.firebaseio.com",
+           projectId: "your-project-id",
+           storageBucket: "your-project-id.appspot.com",
+           messagingSenderId: "sender-id"
+        };
         firebase.initializeApp(config);
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
